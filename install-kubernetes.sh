6b13c4b3ab498b44ef6ebdb1114906abb8c1116a
mohammad rj <39571214+mohammad-rj@users.noreply.github.com>
2023-10-24 09:06:23 +0330

    Update install-kubernetes.sh

--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -9,9 +9,8 @@ sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#/g' /etc/fstab
 sudo setenforce 0
 sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
-sudo firewall-cmd --permanent --add-port=4240/tcp
-sudo firewall-cmd --permanent --add-port=8472/udp
-sudo firewall-cmd --reload
+sudo iptables -A INPUT -p tcp --dport 4240 -j ACCEPT
+sudo iptables -A INPUT -p udp --dport 8472 -j ACCEPT
 sudo modprobe overlay
 sudo modprobe br_netfilter
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
@@ -25,29 +24,27 @@ net.ipv4.ip_forward                 = 1
 EOF
 sudo sysctl --system
 
+# Master node specific settings
+if [ "$isMaster" = "y" ]; then
+  sudo iptables -A INPUT -p tcp --dport 6443 -j ACCEPT
+  sudo iptables -A INPUT -p tcp --dport 2379:2380 -j ACCEPT
+  sudo iptables -A INPUT -p tcp --dport 10250 -j ACCEPT
+  sudo iptables -A INPUT -p tcp --dport 10259 -j ACCEPT
+  sudo iptables -A INPUT -p tcp --dport 10257 -j ACCEPT
+fi
+
+# Worker node specific settings
+if [ "$isMaster" = "n" ]; then
+  sudo iptables -A INPUT -p tcp --dport 10250 -j ACCEPT
+  sudo iptables -A INPUT -p tcp --dport 30000:32767 -j ACCEPT
+fi
+
 # Save iptables rules
 sudo iptables-save > /etc/sysconfig/iptables
 
 # Ensure the rules are restored at boot
 echo "iptables-restore < /etc/sysconfig/iptables" | sudo tee -a /etc/rc.d/rc.local
 sudo chmod +x /etc/rc.d/rc.local
-
-# Master node specific commands
-if [ "$isMaster" = "y" ]; then
-  sudo firewall-cmd --permanent --add-port=6443/tcp
-  sudo firewall-cmd --permanent --add-port=2379-2380/tcp
-  sudo firewall-cmd --permanent --add-port=10250/tcp
-  sudo firewall-cmd --permanent --add-port=10259/tcp
-  sudo firewall-cmd --permanent --add-port=10257/tcp
-  sudo firewall-cmd --reload
-fi
-
-# Worker node specific commands
-if [ "$isMaster" = "n" ]; then
-  sudo firewall-cmd --permanent --add-port=10250/tcp
-  sudo firewall-cmd --permanent --add-port=30000-32767/tcp
-  sudo firewall-cmd --reload
-fi
 
 # Install CRI-O (Automatically fetch the latest version)
 VERSION=$(curl -s https://api.github.com/repos/cri-o/cri-o/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c 2-)
@@ -69,3 +66,4 @@ exclude=kubelet kubeadm kubectl
 EOF
 sudo dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 sudo systemctl enable --now kubelet
+
