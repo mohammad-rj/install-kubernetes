7726154568a9fefdb7a354407575be2f7e2110a6
mohammad rj <39571214+mohammad-rj@users.noreply.github.com>
2023-10-24 08:56:54 +0330

    Update install-kubernetes.sh

--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Prompt for master or worker node
 echo -n "Is this a master node? (y/n): "
 read isMaster
 
@@ -8,32 +9,54 @@ sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#/g' /etc/fstab
 sudo setenforce 0
 sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
-
-sudo iptables -A INPUT -p tcp --dport 4240 -j ACCEPT
-sudo iptables -A INPUT -p udp --dport 8472 -j ACCEPT
-
+sudo firewall-cmd --permanent --add-port=4240/tcp
+sudo firewall-cmd --permanent --add-port=8472/udp
+sudo firewall-cmd --reload
+sudo modprobe overlay
+sudo modprobe br_netfilter
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
-
-sudo modprobe overlay
-sudo modprobe br_netfilter
-
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
-
 sudo sysctl --system
 
-VERSION=1.22
+# Save iptables rules
+sudo iptables-save > /etc/sysconfig/iptables
+
+# Ensure the rules are restored at boot
+echo "iptables-restore < /etc/sysconfig/iptables" | sudo tee -a /etc/rc.d/rc.local
+sudo chmod +x /etc/rc.d/rc.local
+
+# Master node specific commands
+if [ "$isMaster" = "y" ]; then
+  sudo firewall-cmd --permanent --add-port=6443/tcp
+  sudo firewall-cmd --permanent --add-port=2379-2380/tcp
+  sudo firewall-cmd --permanent --add-port=10250/tcp
+  sudo firewall-cmd --permanent --add-port=10259/tcp
+  sudo firewall-cmd --permanent --add-port=10257/tcp
+  sudo firewall-cmd --reload
+fi
+
+# Worker node specific commands
+if [ "$isMaster" = "n" ]; then
+  sudo firewall-cmd --permanent --add-port=10250/tcp
+  sudo firewall-cmd --permanent --add-port=30000-32767/tcp
+  sudo firewall-cmd --reload
+fi
+
+# Install CRI-O (Automatically fetch the latest version)
+VERSION=$(curl -s https://api.github.com/repos/cri-o/cri-o/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c 2-)
 sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/CentOS_8/devel:kubic:libcontainers:stable.repo
-sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:ALL:cri-o:${VERSION}.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:${VERSION}/CentOS_8/devel:kubic:libcontainers:stable:cri-o:${VERSION}.repo
+sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:${VERSION}.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:${VERSION}/CentOS_8/devel:kubic:libcontainers:stable:cri-o:${VERSION}.repo
 sudo dnf -y install cri-o cri-tools
 sudo systemctl enable --now crio
 
+# Install Kubernetes
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -44,21 +67,5 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 exclude=kubelet kubeadm kubectl
 EOF
-
 sudo dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 sudo systemctl enable --now kubelet
-
-# Master node specific commands
-if [ "$isMaster" = "y" ]; then
-  sudo iptables -A INPUT -p tcp --dport 6443 -j ACCEPT
-  sudo iptables -A INPUT -p tcp --dport 2379:2380 -j ACCEPT
-  sudo iptables -A INPUT -p tcp --dport 10250 -j ACCEPT
-  sudo iptables -A INPUT -p tcp --dport 10259 -j ACCEPT
-  sudo iptables -A INPUT -p tcp --dport 10257 -j ACCEPT
-fi
-
-# Worker node specific commands
-if [ "$isMaster" = "n" ]; then
-  sudo iptables -A INPUT -p tcp --dport 10250 -j ACCEPT
-  sudo iptables -A INPUT -p tcp --dport 30000:32767 -j ACCEPT
-fi
