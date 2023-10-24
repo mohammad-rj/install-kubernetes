b167e717e263ccabdec3eecc5a5a7c1c9eddd987
mohammad rj <39571214+mohammad-rj@users.noreply.github.com>
2023-10-24 10:11:03 +0330

    Update install-kubernetes.sh

--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -14,12 +14,6 @@ done
 # Update and upgrade packages
 sudo yum -y update && sudo yum -y upgrade
 
-# Install Docker
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io
-sudo systemctl enable --now docker
-
 # Common settings for all nodes (master + workers)
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#/g' /etc/fstab
